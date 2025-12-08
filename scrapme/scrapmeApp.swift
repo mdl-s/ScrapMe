26c26afbe52c3acf9f4445a2a91d8a10404d5104
Michael Slimani <michael.slimani@live.fr>
2025-12-08 09:22:25 +0100

    Initial Commit

---
+++ scrapme/scrapmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrapmeApp.swift
+//  scrapme
+//
+//  Created by Michael Slimani on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct scrapmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
